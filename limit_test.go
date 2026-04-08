35b1f321ef70bc3ae3ad1b03916185f2e261f67c
go-yaml <yaml@go-yaml.in>
2026-04-08 07:46:21 +0000

    go-yaml.in v4.0.0-rc.1

--- limit_test.go
+++ limit_test.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"testing"
 
-	"go.yaml.in/yaml/v4"
+	"go-yaml.in/yaml/v4"
 )
 
 var limitTests = []struct {
--- node_test.go
+++ node_test.go
@@ -25,7 +25,7 @@ import (
 	"strings"
 	"testing"
 
-	"go.yaml.in/yaml/v4"
+	"go-yaml.in/yaml/v4"
 )
 
 var nodeTests = []struct {
